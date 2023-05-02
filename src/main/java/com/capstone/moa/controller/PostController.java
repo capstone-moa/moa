6ba832e13ff487fb8191491490338ec5bdc8d33c
delay <delay@woowahan.com>
2023-05-03 01:46:49 +0900

    feat: 모든 게시글을 조회하는 API 구현

--- src/main/java/com/capstone/moa/controller/PostController.java
+++ src/main/java/com/capstone/moa/controller/PostController.java
@@ -1,17 +1,19 @@
 package com.capstone.moa.controller;
 
+import com.capstone.moa.dto.FindPostsResponse;
 import com.capstone.moa.dto.WritePostRequest;
 import com.capstone.moa.service.PostService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/post")
+@RequestMapping("/posts")
 public class PostController {
 
     private final PostService postService;
@@ -20,5 +22,10 @@ public class PostController {
     public ResponseEntity<Void> writePost(@RequestBody WritePostRequest postRequest) {
         postService.writePost(postRequest);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<FindPostsResponse> findAllPosts() {
+        return ResponseEntity.ok(postService.findAllPosts());
     }
 }
