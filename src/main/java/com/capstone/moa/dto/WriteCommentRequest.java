996ac3dc1ad1d2cefc0f4ac4669506344c8a736d
byulcode <bybit0408@gmail.com>
2023-05-04 21:27:44 +0900

    feat : 댓글을 작성하는 기능 구현

---
+++ src/main/java/com/capstone/moa/dto/WriteCommentRequest.java
@@ -0,0 +1,14 @@
+package com.capstone.moa.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class WriteCommentRequest {
+
+    private String memberId;
+    private String content;
+}
