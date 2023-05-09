3a11f1206eaaf0cfd42f33c012d839e93e056dbd
leegayeon97 <dlrkdus96@gmail.com>
2023-05-09 16:46:52 +0900

    feat : 댓글을 수정하는 기능 구현

--- src/main/java/com/capstone/moa/controller/CommentController.java
+++ src/main/java/com/capstone/moa/controller/CommentController.java
@@ -1,14 +1,12 @@
 package com.capstone.moa.controller;
 
+import com.capstone.moa.dto.ModifyCommentRequest;
 import com.capstone.moa.dto.WriteCommentRequest;
 import com.capstone.moa.service.CommentService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
@@ -20,6 +18,13 @@ public class CommentController {
     @PostMapping("/{postId}/comments")
     public ResponseEntity<Void> writeComment(@PathVariable("postId") Long postId, @RequestBody WriteCommentRequest request) {
         commentService.writeComment(postId, request);
+        return ResponseEntity.noContent().build();
+    }
+
+    /* UPDATE */
+    @PutMapping("/{postId}/comments")
+    public ResponseEntity<Void> updateComment(@PathVariable("postId") Long postId, @RequestBody ModifyCommentRequest request) {
+        commentService.modifyComment(postId, request);
         return ResponseEntity.noContent().build();
     }
 }
