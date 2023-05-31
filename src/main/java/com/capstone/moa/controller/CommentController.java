95f5d6f817ae76c7c3b32c43777a89955c217725
byulcode <bybit0408@gmail.com>
2023-05-31 21:54:44 +0900

    feat : Swagger 적용 및 메소드 설명 추가

--- src/main/java/com/capstone/moa/controller/CommentController.java
+++ src/main/java/com/capstone/moa/controller/CommentController.java
@@ -3,31 +3,34 @@ package com.capstone.moa.controller;
 import com.capstone.moa.dto.ModifyCommentRequest;
 import com.capstone.moa.dto.WriteCommentRequest;
 import com.capstone.moa.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @AllArgsConstructor
 @RequestMapping("/posts")
 public class CommentController {
 
     private final CommentService commentService;
 
+
+    @Operation(summary = "댓글 작성")
     @PostMapping("/{postId}/comments")
     public ResponseEntity<Void> writeComment(@PathVariable("postId") Long postId, @RequestBody WriteCommentRequest request) {
         commentService.writeComment(postId, request);
         return ResponseEntity.noContent().build();
     }
 
-    /* UPDATE */
+    @Operation(summary = "댓글 수정")
     @PutMapping("/{postId}/comments")
     public ResponseEntity<Void> updateComment(@PathVariable("postId") Long postId, @RequestBody ModifyCommentRequest request) {
         commentService.modifyComment(postId, request);
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "댓글 삭제")
     @DeleteMapping("/{postId}/comments/{commentId}")
     public ResponseEntity<Void> deleteComment(
             @PathVariable("postId") Long postId,
