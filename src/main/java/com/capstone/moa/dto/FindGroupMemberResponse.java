0ee2847e0c9d08aabe57e1edb554ce87fb489a6b
byulcode <bybit0408@gmail.com>
2023-09-24 18:44:45 +0900

    refactor : 마이페이지 멤버 프로필 적용

--- src/main/java/com/capstone/moa/dto/FindGroupMemberResponse.java
+++ src/main/java/com/capstone/moa/dto/FindGroupMemberResponse.java
@@ -3,7 +3,7 @@ package com.capstone.moa.dto;
 import com.capstone.moa.entity.GroupMember;
 
 public record FindGroupMemberResponse(
-        Long id, String email, String job, String role, String name, Long memberId
+        Long id, String email, String job, String role, String name, Long memberId, String memberProfile
 ) {
     public static FindGroupMemberResponse from(GroupMember groupMember) {
         return new FindGroupMemberResponse(
@@ -12,7 +12,8 @@ public record FindGroupMemberResponse(
                 groupMember.getMember().getJob().name(),
                 groupMember.getGroupRole().name(),
                 groupMember.getMember().getName(),
-                groupMember.getMember().getId()
+                groupMember.getMember().getId(),
+                groupMember.getMember().getProfile().getProfileName()
         );
     }
 }
